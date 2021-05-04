967bca28926f2a7817b53349886bed8b3a211f45
Fernando dos Reis Santos Filho <fernando.reis@kaffa.com.br>
2021-05-04 09:25:18 -0300

    Lab05

---
+++ lab05/src/mc322/lab05b/Dama.java
@@ -0,0 +1,10 @@
+package mc322.lab05b;
+
+public class Dama extends Peca {
+    
+    Boolean verificarValidade(int FimI, int FimJ)
+    {
+        super.verificarValidade(FimI, FimJ);
+        return true;
+    }
+}
---
+++ lab05/src/mc322/lab05b/Peao.java
@@ -0,0 +1,11 @@
+package mc322.lab05b;
+
+public class Peao extends Peca {
+
+    Boolean verificarValidade(int FimI, int FimJ)
+    {
+        super.verificarValidade(FimI, FimJ);
+        return true;
+    }
+
+}
